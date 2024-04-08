9d58740afe42080aeda02b0cc2a3ea127d37fd05
Marco <marcocampanale314@gmail.com>
2024-04-08 12:31:55 +0200

    push

--- query fucino.sql
+++ query fucino.sql
@@ -104,7 +104,7 @@ AND COD_NDG_DEBI = 1000681
 -- ffgt_valparls (valori) ticket 2024/40 fucino
 
 
---ticker 2024/65
+--ticker 2024/65 e 2024/112 Mappatura NDG K4F con NDG Cedacri 
 SELECT a.cod_sogg_codi, a.cod_estr, b.des_sogg_uten  FROM gagt_codieste a , gagt_ndg b
 where a.cod_sogg_codi = b.cod_soggetto
 and cod_tipo_codi=100
